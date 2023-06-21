f3751123a4beaf2d7057c0abc63c97b4e0c3ba86
mitko <petrovmitko@outlook.com>
2023-06-21 17:09:41 +0300

    config setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDpmJvQmtMF0yOdATjzXs2S1i8WOUKI7uQ",
+    authDomain: "starwars-cbf47.firebaseapp.com",
+    projectId: "starwars-cbf47",
+    storageBucket: "starwars-cbf47.appspot.com",
+    messagingSenderId: "472971824723",
+    appId: "1:472971824723:web:292acd574854373e22c0a6"
+  }
 };
